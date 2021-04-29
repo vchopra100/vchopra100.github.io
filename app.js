29dff710bd7939e93c66f4b8b525b8194e93f5af
vchopra100 <vedantchopra@hotmail.com>
2021-04-28 21:35:22 -0500

    More edits (non Abby proofread)

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = "#116c84";
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
